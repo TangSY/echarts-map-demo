3b20e7e2d3de9cc25faacbd2b19e8bd74b746c53
TSY <t@tsy6.com>
2018-12-06 23:14:48 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+/**
+* @Description:    通知公告人员选择
+* @Author:         TSY
+* @CreateDate:     2018/9/5 9:04
+* @email:          t@tsy6.com
+*/
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "demo",
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import "./style/reset.styl"
+    @import "./style/common.styl"
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+/**
+ * @Description:    自定义配置
+ * @Author:         TSY
+ * @CreateDate:     2018/6/9 13:28
+ */
+
+module.exports = {
+    baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+}
