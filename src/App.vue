d137f59a8ae19b05877ea3285af07272a8108593
TSY <t@tsy6.com>
2019-05-09 23:27:43 +0800

    opt 优化文件目录结构

--- src/App.vue
+++ src/App.vue
@@ -12,31 +12,11 @@
         <div class="echarts"> 
             <div id="map"></div>
         </div>
-        <div class="dialog" v-show="isShowDialog">
-            <div class="dialog-content">
-                <div class="dialog-title">赏根辣条？</div>
-                <div class="dialog-close" @click="closeDialog">&times;</div>
-                <div class="dialog-img">
-                    <img src="./images/alipay.png" alt="">
-                    <img src="./images/wxpay.jpg" alt="">
-                </div>
-                <div class="dialog-bottom">
-                    <div class="dialog-btn dialog-cancel" @click="confirmDownload">我就不！傲娇地下载</div>
-                    <div class="dialog-btn dialog-confirm" @click="confirmDownload">已打赏！壕气地下载</div>
-                </div>
-            </div>
-        </div>
         <div class="tips" v-show="isShowTips">正在下载，请耐心等待。。。</div>
-        <a href="https://github.com/TangSY/echarts-map-demo" class="github-corner" aria-label="View source on GitHub" target="_blank">
-            <svg width="80" height="80" viewBox="0 0 250 250"
-                 style="fill:#FD6C6C; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
-                <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
-                <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
-                      fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
-                <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
-                      fill="currentColor" class="octo-body"></path>
-            </svg>
-        </a>
+        <!--哎呀呀，这就是打赏弹窗，为方便你们删除，就单独抽出一个组件来吧-->
+        <money-dialog ref="dialog" @confirm="downloadAllJson"></money-dialog>
+        <!--github入口-->
+        <github></github>
     </div>
 </template>
 
@@ -44,10 +24,15 @@
     import JSZip from 'jszip'
     import saveAs from './saveAs'
     import MapRange from "./MapRange";
+    import MoneyDialog from "./MoneyDialog";
+    import Github from "./Github";
 
     export default {
         name: "demo",
-        components: {MapRange},
+        components: {
+            Github,
+            MoneyDialog,
+            MapRange},
         data() {
             return {
                 cityName: '中国',
@@ -68,8 +53,7 @@
                 codeList: [],
                 isCodeListLoadComplete: false,//codeList是否全部获取完毕
                 downloadTips: '下载geoJson数据',//下载进度提示
-                isShowDialog: false,//是否显示打赏弹窗
-                isShowTips: false,//是否下载提示
+                isShowTips: false,//是否显示下载提示
             }
         },
         mounted() {
@@ -155,7 +139,7 @@
             downloadJson(nameType) {//geo文件下载
                 if (nameType === 'all') {
                     this.$ba.trackEvent('echartsMap', '文件下载', '打包下载全部');
-                    this.isShowDialog = true;
+                    this.$refs.dialog.show();
                     return;
                 }
                 var blob = new Blob([JSON.stringify(this.geoJsonData)], {type: "text/plain;charset=utf-8"});
@@ -165,13 +149,6 @@
                 }
                 this.$ba.trackEvent('echartsMap', '文件下载', filename);
                 saveAs(blob, `${filename}.geoJson`);//filename
-            },
-            closeDialog() {
-                this.isShowDialog = false;
-            },
-            confirmDownload() {
-                this.downloadAllJson();
-                this.isShowDialog = false;
             },
             downloadAllJson() {//一次打包下载所有的数据
                 this.showTips();
@@ -458,95 +435,6 @@
         height 100vh
     }
 
-    .dialog {
-        position fixed
-        top 0px
-        left 0px
-        width 100%
-        height 100%
-        display flex
-        align-items center
-        justify-content center
-        background rgba(0, 0, 0, .3)
-        z-index 999
-    }
-
-    .dialog-content {
-        position relative
-        width 600px
-        display flex
-        align-items center
-        flex-direction column
-        background #17c1fb
-        padding 30px
-        border-radius 10px
-        color #fff
-    }
-
-    .dialog-title {
-        font-size 24px
-        margin-bottom 15px
-    }
-
-    .dialog-close {
-        font-size 26px
-        position absolute
-        right 20px
-        top 0px
-        cursor pointer
-    }
-
-    .dialog-img {
-        width 100%
-        display flex
-        align-items center
-        justify-content space-around
-    }
-
-    .dialog-img img {
-        width 250px
-        height 350px
-    }
-
-    .dialog-bottom {
-        width 100%
-        margin-top 40px
-        display flex
-        align-items center
-        justify-content space-around
-    }
-
-    .dialog-btn {
-        position relative
-        padding 10px 20px
-        background #1c71fb
-        margin-bottom 15px
-        border-radius 5px
-        cursor pointer
-    }
-
-    .dialog-cancel {
-        &:hover {
-            &:before {
-                position absolute
-                left -170px
-                bottom -90px
-                content url("./images/aojiao.gif")
-            }
-        }
-    }
-
-    .dialog-confirm {
-        &:hover {
-            &:before {
-                position absolute
-                right -190px
-                bottom -90px
-                content url("./images/love.jpg")
-            }
-        }
-    }
-
     .tips {
         position fixed
         bottom 30%
@@ -556,31 +444,5 @@
         color #fff
         background rgba(0, 0, 0, .8)
         z-index 999
-    }
-
-    .github-corner:hover .octo-arm {
-        animation: octocat-wave 560ms ease-in-out
-    }
-
-    @keyframes octocat-wave {
-        0%, 100% {
-            transform: rotate(0)
-        }
-        20%, 60% {
-            transform: rotate(-25deg)
-        }
-        40%, 80% {
-            transform: rotate(10deg)
-        }
-    }
-
-    @media (max-width: 500px) {
-        .github-corner:hover .octo-arm {
-            animation: none
-        }
-
-        .github-corner .octo-arm {
-            animation: octocat-wave 560ms ease-in-out
-        }
     }
 </style>
